29195acffdfccae6347b0a6561c0f26295f0029b
vsync <vsync.design@gmail.com>
2014-10-28 22:03:27 +0200

    -

--- infinite.js
+++ infinite.js
@@ -15,7 +15,7 @@
     var defaults = {
         startIndex : 0,    // the first index to render
         pageSize   : 10,   // # of items in a single batch
-        offset     : 12,   // the area from top and bottom (in %) of after which to trigger new page render
+        offset     : 10,   // the area from top and bottom (in %) of after which to trigger new page render
         content    : null, // where the items should be appended to
         newPage    : null,  // function which generates a whole page and returns it (as a jQuery object)
         fakeHeight : false
@@ -37,8 +37,8 @@
     function Infinite($el, settings, cb){
         var that = this;
 
-        this.el = $el;
-        this.endlessElm = ($el[0] == window) ? $(docElm) : $el;
+        this.el               = $el;
+        this.endlessElm       = ($el[0] == window) ? $(docElm) : $el;
         this.endlessContainer = settings.content || this.endlessElm;
 
         this.index             = settings.fakeHeight ? 0 : settings.startIndex || 0;
@@ -66,11 +66,15 @@
         // Addes a new page to the DOM
         // Gets new elements Array from the "newPage" function and use the result.
         addPage : function( method ){
+            if( this.locked ) return;
+            this.locked = true;
+
             var index = this.index,
                 newItems, elmToRemove, height = 0, temp, parsedPadding,
                 N = this.settings.pageSize; // number of items to generate
 
             method = method || 'append';
+            console.log(method);
 
             //////////////////////////////
             // appending elements
@@ -79,7 +83,10 @@
 
                 newItems = this.settings.newPage.call(this);
 
-                if( !newItems ) return;
+                if( !newItems ){
+                    this.locked = false;
+                    return;
+                }
 
                 if( temp.length >= this.settings.pageSize * 2 )
                     elmToRemove = temp.slice(0, newItems.length); // remove the same amount of items that were added
@@ -96,33 +103,49 @@
                         this.fakeScrolls[1] = this.fakeScrolls[1] > height ? this.fakeScrolls[1] - height : 0;
                     }
                     else{
-                        temp = elmToRemove.last();
-                        height = temp.position().top + temp.outerHeight(true);
+                        // add new items
+                        this.endlessContainer[method]( newItems );
+                        // save the scrollHeight
+                        height = this.endlessContainer[0].scrollHeight;
+                        // remove un-needed items
+                        elmToRemove.remove();
+                        // re-calculate the height and get the delta from before
+                        height = height - this.endlessContainer[0].scrollHeight;
+                        // scroll back up by the delta previously calculated
 
                         if( this.el[0] === window ){
                             docElm.scrollTop -= height;
                             document.body.scrollTop -= height;
                         }
-                        else
+                        else{
+                            console.log(height);
                             this.endlessElm[0].scrollTop -= height;
+                        }
+
                     }
-
-                    elmToRemove.remove();
                 }
-
-                this.endlessContainer[method]( newItems );
+                else
+                    this.endlessContainer[method]( newItems );
             }
 
             //////////////////////////////
             // prepending elements
             else{
-                if( index < 0 ) return;
+                if( index < 0 ){
+                    this.locked = false;
+                    return;
+                };
 
                 N = this.firstLastIndexes[0] < this.settings.pageSize ? this.firstLastIndexes[0] : this.settings.pageSize;
 
                 newItems = this.settings.newPage.call(this, N);
 
-                if( !newItems ) return;
+                if( !newItems ){
+                    this.locked = false;
+                    return;
+                };
+
+                height = this.endlessContainer[0].scrollHeight;
 
                 this.endlessContainer[method]( newItems );
 
@@ -137,8 +160,9 @@
                     this.fakeScrolls[1] += height;
                 }
                 else{
-                    temp = $( newItems[newItems.length - 1] );
-                    height = temp.position().top + temp.outerHeight(true);
+                    //temp = $( newItems[newItems.length - 1] );
+                    //height = temp.position().top + temp.outerHeight(true);
+                    height = this.endlessContainer[0].scrollHeight - height;
 
                     if( this.el[0] === window ){
                         docElm.scrollTop += height;
@@ -160,32 +184,56 @@
             temp = this.endlessContainer.children();
             this.firstLastIndexes = [temp[0].tabIndex, temp.last()[0].tabIndex];
             this.firstLastElements = [temp[0], temp.slice(-1)[0]];
+
+            this.locked = false;
+
+            if( this.settings.afterCB )
+                this.settings.afterCB(newItems);
         },
 
+        goTo : function(index){
+            var toRemove = this.endlessContainer.children().remove();
+            this.locked = false;
+            this.index = index;
+            this.addPage();
+        },
 
         // onScroll/resize events callback
         // calls "addPage". Knows wether to Append or Prepend content
-        scrolling : function(){
+        scrolling : (function(){
+            var timer;
+
+            return function(){
+                clearTimeout(timer);
+                this.checkEdges();
+                var that = this;
+                // if scrolled down too fast, do a delayed check of position
+                timer = setTimeout(function(){
+                    that.lastScrollTop = 0;
+                    that.checkEdges();
+                },200);
+            }
+        })(),
+
+        checkEdges : function(){
             var st = this.el.scrollTop(),
                 isScrollingDown = st > this.lastScrollTop,
-                temp, pageToRestore, viewHeight, totalHeight;
+                temp, pageToRestore, viewHeight, totalHeight, bottomDistancePercent, topDistancePercent;
 
             this.lastScrollTop = st;
             viewHeight = this.endlessElm[0].clientHeight;
 
-            // scrolling down
-            if( isScrollingDown ){
-                temp = this.firstLastElements[1].getBoundingClientRect().top;
-                // if reached NEXT page loading point
-                //if( this.scrollY + viewHeight + this.settings.offset >= (totalHeight - this.fakeScrolls[1]) ){
-                if( viewHeight / temp > (1 - this.settings.offset / 100) || temp < viewHeight ){
-                    this.index = this.firstLastIndexes[1] + 1;
-                    this.addPage();
-                }
+            bottomDistancePercent = 100 - (st + viewHeight) / this.endlessElm[0].scrollHeight * 100;
+
+            // if reached NEXT page loading point
+            //if( this.scrollY + viewHeight + this.settings.offset >= (totalHeight - this.fakeScrolls[1]) ){
+            if( isScrollingDown && bottomDistancePercent < this.settings.offset ){
+                this.index = this.firstLastIndexes[1] + 1;
+                this.addPage();
             }
             // scrolling up
             //else if( (this.scrollY - this.fakeScrolls[0]) <= this.settings.offset && this.index ){
-            else if( this.firstLastElements[0].getBoundingClientRect().bottom / -viewHeight < (this.settings.offset / 100) && this.index ){
+            else if( !isScrollingDown && this.firstLastElements[0].getBoundingClientRect().bottom / -viewHeight < (this.settings.offset / 100) && this.index ){
                 if( this.firstLastIndexes[0] == 0 )
                     return;
 
